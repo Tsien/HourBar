d919628c56d9c060a1654f54f33687af4d87fafe
Tsien <feqian@ucsd.edu>
2017-03-01 20:36:39 -0800

    the bar shows up

--- content.js
+++ content.js
@@ -1,23 +1,29 @@
 
-// get the first element in DOM
-var firstElement = $(document).first();
+(function addBar() {
+	var newDiv = document.createElement("div");
+	var newContent = document.createTextNode("1%");
+	newDiv.appendChild(newContent);
+	newDiv.setAttribute("id", "myBar");
+	var divP = document.createElement("div");
+	divP.appendChild(newDiv);
+	divP.setAttribute("id", "myProgress");
+	document.body.appendChild(divP);
+}).call(this);
 
-var bar = document.createElement("div");
-var 
+(function move() {
+	var elem = document.getElementById("myBar");
+	var width = 1;
+	var id = setInterval(frame, 1000);
+	function frame() {
+		if (width >= 100) {
+			clearInterval(id);
+		}
+		else {
+			width++;
+			elem.style.width = width + "%";
+			elem.innerHTML = width * 1 + "%";
+		}
+	}
+}).call(this);
 
 
-
-/*
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-      console.log(firstHref);
-
-      // This line is new!
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-    }
-  }
-);
-*/
